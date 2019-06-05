a8b8f4c4c28546824ad21caf697f482c8a6bda17
krikaz <pascal.bou@protonmail.com>
2019-06-05 15:20:45 +0200

    built WithAuthenticate component. The username check is hardcoded

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -7,7 +7,7 @@ import PostPages from './components/PostContainer/PostPages';
 import WithAuthenticate from './components/Authentification/WithAuthenticate';
 import Login from './components/Login/Login';
 
-const ComponentFromWithAuthenticate = WithAuthenticate(PostPages);
+const ComponentFromWithAuthenticate = WithAuthenticate(PostPages, Login);
 
 export default class App extends React.Component {
 	constructor(props) {
@@ -21,7 +21,7 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Login />
+				<ComponentFromWithAuthenticate />
 			</div>
 		);
 	}
--- instagram-app/src/components/Authentification/WithAuthenticate.js
+++ instagram-app/src/components/Authentification/WithAuthenticate.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-export default function WithAuthenticate(Component) {
-  return class extends React.Component {
-    render() {
-      return (
-        <Component />
-      );
-    }
-  }
+export default function WithAuthenticate(FirstComponent, SecondComponent) {
+	return class extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = {
+				logginIn: false,
+			};
+		}
+
+		componentDidMount() {
+			if (window.localStorage['user']) {
+				this.setState({ logginIn: true });
+			}
+		}
+
+		render() {
+      if (this.state.logginIn) {
+        return <FirstComponent />;
+      } else {
+		  	return <SecondComponent />;
+      }
+		}
+	};
 }
