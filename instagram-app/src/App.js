6bf7e6c355e31c2b9f127e0cf074d958e1964da1
krikaz <pascal.bou@protonmail.com>
2019-06-06 14:33:29 +0200

    imported styled-components, removed app.css not used

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import PostPages from './components/PostContainer/PostPages';
 import WithAuthenticate from './components/Authentification/WithAuthenticate';
 import Login from './components/Login/Login';
@@ -7,11 +6,6 @@ import Login from './components/Login/Login';
 const ComponentFromWithAuthenticate = WithAuthenticate(PostPages)(Login);
 
 export default class App extends React.Component {
-	// constructor(props) {
-	// 	super(props);
-	// 	this.state = {};
-	// }
-
 	render() {
 		return (
 			<div>
