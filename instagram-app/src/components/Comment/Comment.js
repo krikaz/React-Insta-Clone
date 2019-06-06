ff1fc30dff41eac060e71e18050d724aea57526b
krikaz <pascal.bou@protonmail.com>
2019-06-06 14:46:22 +0200

    replaced Comment.css with styled-comp

--- instagram-app/src/components/Comment/Comment.js
+++ instagram-app/src/components/Comment/Comment.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Comment.css';
+import styled from 'styled-components';
+
+const Span = styled.span`
+	font-weight: bold;
+`;
+
+const CommentDiv = styled.div`
+	margin-bottom: 0.5rem;
+`;
 
 export default function Comment({ username, text }) {
-
 	return (
-		<div className='comment'>
-			<span className='username'>{username}</span>
-			<span className='text'> {text}</span>
-		</div>
+		<CommentDiv>
+			<Span>{username}</Span>
+			<span> {text}</span>
+		</CommentDiv>
 	);
 }
 
